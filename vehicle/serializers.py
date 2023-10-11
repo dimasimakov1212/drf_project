12b987569e7336372b2044fa3961895e4ed7e3f5
DimaSimakov <dimarodeo@gmail.com>
2023-10-11 23:29:43 +0700

    Add convert_currencies

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from vehicle.models import Car, Moto, Milage
+from vehicle.services import convert_currencies
 from vehicle.validators import TitleValidator
 
 
@@ -18,14 +19,19 @@ class CarSerializer(serializers.ModelSerializer):
     Класс сериализатора для модели Car
     """
     # определяем дополнительное поле в модели Car
-    last_milage = serializers.IntegerField(source='milage_set.all.first.milage', read_only=True)  # данные о пробеге
+    last_milage = serializers.IntegerField(source='milage.all.first.milage', read_only=True)  # данные о пробеге
     # milage = MilageSerializer(source='milage_set', many=True)
     # параметр source теперь не нужен, т.к. в модели определили related_name='milage'
     milage = MilageSerializer(many=True, read_only=True)
 
+    usd_price = serializers.SerializerMethodField()  # определяем поле вывода стоимости авто в долларах
+
     class Meta:
         model = Car
         fields = '__all__'
+
+    def get_usd_price(self, instance):
+        return convert_currencies(instance.amount)
 
 
 class MotoSerializer(serializers.ModelSerializer):
