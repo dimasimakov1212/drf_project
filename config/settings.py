503a1717ec7d0cffcb4958a80b5dcdd0c7e5ccd0
DimaSimakov <dimarodeo@gmail.com>
2023-11-04 02:10:02 +0700

    docker

--- config/settings.py
+++ config/settings.py
@@ -96,16 +96,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'drf_project',
+#         'USER': 'postgres',
+#         'PASSWORD': 'dima123',
+#         'HOST': '127.0.0.1'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'drf_project',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'dima123',
-        'HOST': '127.0.0.1'
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
